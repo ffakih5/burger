7c742e5f84697cd039583790e7a5ea9052d57b5a
Fatima Fakih <fatima.fakih@hotmail.com.au>
2021-04-03 12:40:58 +1100

    burgers_controllers; all burgers

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,2 +1,13 @@
 const express = require('express');
 const burger = require('../models/burger')
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    burger.allBurgers((data) => {
+        const hbsObject = {
+            burgers: data,
+        };
+        res.render('index', hbsObject);
+    });
+});
