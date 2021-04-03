a2b118057b35d80936e1dc5521f0c65b9ff5cf2e
Fatima Fakih <fatima.fakih@hotmail.com.au>
2021-04-03 13:07:38 +1100

    burgers_controllers with ALL, INSERT, UPDATE and router exported

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -11,3 +11,22 @@ router.get('/', (req, res) => {
         res.render('index', hbsObject);
     });
 });
+
+router.post('/api/burgers', (req, res) => {
+    burger.addBurger(req.body.burger_name, (result) => {
+        res.status(200).end();
+    });
+});
+
+router.post('/api/burgers/:id', (req, res) => {
+    burger.updateBurger(req.body.devoured, req.params.id, (result) => {
+        if (result.changedRows === 0) {
+            // If no rows were changed, then the ID must not exist, so 404
+            return res.status(404).end();
+        }
+        res.status(200).end();
+
+    });
+});
+
+module.exports = router;
