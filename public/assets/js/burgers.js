dd484bc21573deb0c8b21ac2b129c3ad01c92024
Fatima Fakih <fatima.fakih@hotmail.com.au>
2021-04-03 15:57:27 +1100

    form name in index handlebars and createBurger in burger.js

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -5,37 +5,66 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
     const devourBtn = document.querySelectorAll('.devour-button');
 
-    if (devourBtn) {
-        devourBtn.forEach((button) => {
-            button.addEventListener('click', (e) => {
-                e.preventDefault();
+    devourBtn.forEach((button) => {
+        button.addEventListener('click', (e) => {
+            e.preventDefault();
 
-                const dataId = e.target.getAttribute('data-id');
-                const id = parseInt(dataId);
+            const dataId = e.target.getAttribute('data-id');
+            const id = parseInt(dataId);
 
-                const isDevoured = {
-                    devoured: true,
-                };
+            const isDevoured = {
+                devoured: true,
+            };
 
-                fetch(`/api/burgers/${id}`, {
-                    method: 'PUT',
-                    headers: {
-                        Accept: 'application/json',
-                        'Content-Type': 'application/json',
-                    },
+            fetch(`/api/burgers/${id}`, {
+                method: 'PUT',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(isDevoured),
+            }).then((response) => {
+                // Empty the form
+                if (response.ok) {
+                    console.log(`Status Updated: ${isDevoured}`);
+                    location.reload('/');
+                } else {
+                    alert('Could not update devour status')
+                }
 
-                })
-            })
+            });
+        });
 
+    });
+
+    const submitBurger = document.getElementById('start-form');
+
+    submitBurger.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const burgerName = document.getElementById('burger-select').value.trim();
+        if (burgerName === '') {
+            return;
         }
+        const newBurger = {
+            burger_name: burgerName
+        };
 
+        fetch('/api/burgers', {
+            method: 'POST',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
 
-
-
-
-
-
-
-
+            // make sure to serialize the JSON body
+            body: JSON.stringify(newBurger),
+        }).then(() => {
+            // Empty the form
+            document.getElementById('burger-select').value = '';
+            // Reload the page so the user can see the new quote
+            console.log('Created a new burger!');
+            location.reload();
+        });
+    });
 
 });
