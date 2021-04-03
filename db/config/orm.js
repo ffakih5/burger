28991731d80c2c709042dc13b584379619317038
Fatima Fakih <fatima.fakih@hotmail.com.au>
2021-04-03 11:38:52 +1100

    orm.js file with sql query methods

--- db/config/orm.js
+++ db/config/orm.js
@@ -1,1 +1,34 @@
 const connection = require('./connection');
+
+const orm = {
+    selectAll(tableInput, cb) {
+        const queryString = 'SELECT * FROM ??';
+        connection.query(queryString, [tableInput],
+            (err, res) => {
+                if (err) throw err;
+                cb(res);
+            }
+        );
+    },
+    insertOne(tableInput, colToSearch, valOfCol, cb) {
+        const queryString = 'INSERT INTO ?? (??) VALUES (?)';
+        connection.queryString(queryString, [tableInput, colToSearch, valOfCol],
+            (err, res) => {
+                if (err) throw err;
+                cb(res);
+            }
+        );
+    },
+    updateOne(tableInput, colToSearch, valOfCol, conOfCol, valOfCon, cb) {
+        const queryString = 'UPDATE ?? SET ?? = ? WHERE ?? = ?';
+        connection.queryString(queryString, [tableInput, colToSearch, valOfCol, conOfCol, valOfCon],
+            (err, res) => {
+                if (err) throw err;
+                cb(res);
+            }
+        );
+    }
+};
+
+module.exports = orm;
+
