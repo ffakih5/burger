5c220394d624db9e48e6672087b9830ddbe8fdda
Fatima Fakih <fatima.fakih@hotmail.com.au>
2021-04-03 15:10:37 +1100

    adding event listener click to devoure buttons and change to devoured

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -2,3 +2,40 @@ document.addEventListener('DOMContentLoaded', (event) => {
     if (event) {
         console.info('DOM Loaded');
     }
+
+    const devourBtn = document.querySelectorAll('.devour-button');
+
+    if (devourBtn) {
+        devourBtn.forEach((button) => {
+            button.addEventListener('click', (e) => {
+                e.preventDefault();
+
+                const dataId = e.target.getAttribute('data-id');
+                const id = parseInt(dataId);
+
+                const isDevoured = {
+                    devoured: true,
+                };
+
+                fetch(`/api/burgers/${id}`, {
+                    method: 'PUT',
+                    headers: {
+                        Accept: 'application/json',
+                        'Content-Type': 'application/json',
+                    },
+
+                })
+            })
+
+        }
+
+
+
+
+
+
+
+
+
+
+});
